246578baa790da0ad6ac9486b6e301c3400e4ee2
Adrian Serrano <adrisr83@gmail.com>
2018-05-03 19:47:29 +0200

    Initial Commit

---
+++ FGMouseHelper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FGMouseHelper
+//
+//  Created by Adrian Serrano on 03/05/2018.
+//  Copyright © 2018 Elastic. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ FGMouseHelper/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  FGMouseHelper
+//
+//  Created by Adrian Serrano on 03/05/2018.
+//  Copyright © 2018 Elastic. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
